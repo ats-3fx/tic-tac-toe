3dc3e4b3e9758a92cad8d70e77b4fc8e1cd05d92
ats-3fx <atsushi.kawano@techpark.ed.jp>
2022-02-25 22:43:13 +0900

    線で区分わけをした

--- main.js
+++ main.js
@@ -2,6 +2,22 @@ const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 const w = 500;
 const h = 500;
+const margin = 50;
+const wL = (w - 2 * margin);
+const wl = wL/3;
+const hL = (h - 2 * margin);
+const hl = hL/3;
 
 ctx.fillStyle = "rgb(0, 120, 210)";
 ctx.fillRect(0, 0, w, h);
+
+ctx.beginPath();
+ctx.moveTo(margin + wl, margin);
+ctx.lineTo(margin + wl, h - margin);
+ctx.moveTo(margin + wl * 2, margin);
+ctx.lineTo(margin + wl * 2, h - margin);
+ctx.moveTo(margin, margin + hl);
+ctx.lineTo(w - margin, margin + hl);
+ctx.moveTo(margin, margin + hl * 2);
+ctx.lineTo(w - margin, margin + hl * 2);
+ctx.stroke();
