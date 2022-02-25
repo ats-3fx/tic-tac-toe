64a4d3845e334148a384113e438f0c802ba69de3
ats-3fx <atsushi.kawano@techpark.ed.jp>
2022-02-25 20:02:38 +0900

    背景を設定した

---
+++ main.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+const w = 500;
+const h = 500;
+
+ctx.fillStyle = "rgb(0, 120, 210)";
+ctx.fillRect(0, 0, w, h);
